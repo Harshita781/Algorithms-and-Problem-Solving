7a1d8d9846209e14a557665aaf80732336f46d8b
Harshita781 <sharmaharshita431@gmail.com>
2022-09-17 19:31:15 +0530

    initial commit

---
+++ Q-04.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+void sort012(int *arr,int *arr2,int n)
+{
+    int c0=0,c1=0,c2=0;
+    for(int i=0;i<n;i++){
+         if(arr[i]==0){
+            c0++; 
+        
+        }
+        else if(arr[i]==1){
+            c1++;
+        }
+        else{
+            c2++;
+        }
+    }  
+    for(int i=0;i<n;i++){
+        if(c0!=0){
+            arr2[i]=0;
+        }
+        else if(c1!=0){
+            arr2[i]=1;
+        }
+        else{
+            arr2[i]=2;
+        }
+    }
+}
+void print(int arr2[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr2[i];
+    }
+}
+int main(){
+    int n=10;
+    int *arr=new int [n];;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int *arr2=new int [n];
+    sort012(arr,arr2,n);
+    print(arr2,n);
+    return 0;
+}
