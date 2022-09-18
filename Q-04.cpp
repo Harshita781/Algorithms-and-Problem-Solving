cab79ebed0c7e0579ae16efa227d714ec5513671
Harshita781 <sharmaharshita431@gmail.com>
2022-09-18 21:44:15 +0530

    initial commit

--- Q-04.cpp
+++ Q-04.cpp
@@ -1,3 +1,11 @@
+/*
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+*/
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
