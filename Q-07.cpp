89de6ef388cb4db6d36f463434c6e3770001d1da
Harshita781 <sharmaharshita431@gmail.com>
2022-09-21 20:06:33 +0530

    initial

--- Q-07.cpp
+++ Q-07.cpp
@@ -1,3 +1,12 @@
+/*
+Given an array, rotate the array by one position in clock-wise direction.
+
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+*/
 #include <iostream>
 using namespace std;
 void rotate(int arr[], int n)
