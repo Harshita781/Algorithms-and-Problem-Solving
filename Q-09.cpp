8169e11e0191e247c3e6ed4ed313938d347092a5
Harshita781 <sharmaharshita431@gmail.com>
2022-09-23 23:01:02 +0530

    initial

--- Q-09.cpp
+++ Q-09.cpp
@@ -1,3 +1,21 @@
+/*
+Given an array arr[] denoting heights of N towers and a positive integer K.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by K
+Decrease the height of the tower by K
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+nput:
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{3, 3, 6, 8}. The difference between 
+the largest and the smallest is 8-3 = 5.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
