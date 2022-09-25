61a3862a0fee049a0eaa96c7ad7f93a661b0997c
Harshita781 <sharmaharshita431@gmail.com>
2022-09-25 19:33:20 +0530

    initial

--- Q-09.cpp
+++ Q-09.cpp
@@ -12,8 +12,8 @@ Arr[] = {1, 5, 8, 10}
 Output:
 5
 Explanation:
-The array can be modified as 
-{3, 3, 6, 8}. The difference between 
+The array can be modified as
+{3, 3, 6, 8}. The difference between
 the largest and the smallest is 8-3 = 5.
 */
 #include <bits/stdc++.h>
@@ -23,16 +23,17 @@ int getMinDiff(int arr[], int n, int k)
 {
     sort(arr, arr + n);
     int ans = arr[n - 1] - arr[0];
-  
+
     int tempmin, tempmax;
     tempmin = arr[0];
     tempmax = arr[n - 1];
-  
-    for (int i = 1; i < n; i++) {
-  
+
+    for (int i = 1; i < n; i++)
+    {
+
         if (arr[i] - k < 0)
             continue;
-  
+
         tempmin = min(arr[0] + k, arr[i] - k);
         tempmax = max(arr[i - 1] + k, arr[n - 1] - k);
         ans = min(ans, tempmax - tempmin);
@@ -42,9 +43,9 @@ int getMinDiff(int arr[], int n, int k)
 
 int main()
 {
-  
+
     int k = 6, n = 6;
-    int arr[n] = { 7, 4, 8, 8, 8, 9 };
+    int arr[n] = {7, 4, 8, 8, 8, 9};
     int ans = getMinDiff(arr, n, k);
     cout << ans;
 }
