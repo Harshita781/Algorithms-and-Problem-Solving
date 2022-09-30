7cd0eb1b1b3057256da18a7e87b65df1b928b4af
Harshita781 <sharmaharshita431@gmail.com>
2022-09-30 18:18:15 +0530

    initial

--- Q-10.cpp
+++ Q-10.cpp
@@ -1,20 +1,34 @@
-#include<iostream>
-#include<vector>
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+*/
+#include <iostream>
+#include <vector>
 using namespace std;
-bool canJump(vector<int>& nums) {
-        
-        int reachable=0;
-        for(int i=0;i<nums.size();++i){
-            if(reachable<i)
-                return 0;
-            reachable=max(reachable,i+nums[i]);
-            
-        }
-        return reachable;
-        
+bool canJump(vector<int> &nums)
+{
+
+    int reachable = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (reachable < i)
+            return 0;
+        reachable = max(reachable, i + nums[i]);
     }
-int main(){
-     vector <int> nums {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
-     cout<<canJump(nums);
-     return 0;
-     }
+    return reachable;
+}
+int main()
+{
+    vector<int> nums{1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+    cout << canJump(nums);
+    return 0;
+}
--- Q-11.cpp
+++ Q-11.cpp
@@ -1,1 +1,21 @@
-#include<
+#include<iostream>
+using namespace std;
+void mergesortedarray(int a[],int b[],int n, int m){
+    int k=m+n;
+    for(int i=0;i<k;i++){
+        for(int j=0,l=0;j<n,l<m;j++,l++){
+            if(a[j]<=b[l]){
+                a[k]=a[j];
+            }
+            else{
+                a[k]=b[l]
+            }
+        }   
+    }
+}
+void print(int a[]){
+    for(int)
+}
+int main(){
+
+}
