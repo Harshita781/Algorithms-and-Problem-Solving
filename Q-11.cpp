675b93d5c7a968a13deacf9edb3e89e331a28e58
Harshita781 <sharmaharshita431@gmail.com>
2022-10-01 20:46:36 +0530

    initial

--- Q-11.cpp
+++ Q-11.cpp
@@ -1,21 +1,35 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and uses only constant extra space.
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+*/
 #include<iostream>
+#include<vector>
 using namespace std;
-void mergesortedarray(int a[],int b[],int n, int m){
-    int k=m+n;
-    for(int i=0;i<k;i++){
-        for(int j=0,l=0;j<n,l<m;j++,l++){
-            if(a[j]<=b[l]){
-                a[k]=a[j];
-            }
-            else{
-                a[k]=b[l]
-            }
-        }   
+int findDuplicate(vector<int>& nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } 
+  while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
     }
-}
-void print(int a[]){
-    for(int)
-}
 int main(){
-
-}
+    vector <int> nums {1,2,3,4,5,2,6};
+    cout<<findDuplicate(nums);
+    return 0;
+    }
+    
