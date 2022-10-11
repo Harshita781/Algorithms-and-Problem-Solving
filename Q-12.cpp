4d43a9ccb9af91e055a20d656e32b76f8570ac11
Harshita781 <sharmaharshita431@gmail.com>
2022-10-11 22:22:36 +0530

    initial

--- Q-12.cpp
+++ Q-12.cpp
@@ -1,27 +1,37 @@
-#include<bits/stdc++.h>
+/*
+We are given two sorted arrays. We need to merge these two arrays such that the initial numbers (after complete sorting) are in the first array and the remaining numbers are in the second array. Extra space is allowed in O(1).
+
+Example: 
+Input: ar1[] = {10};
+       ar2[] = {2, 3};
+Output: ar1[] = {2}
+        ar2[] = {3, 10} 
+*/
+#include <bits/stdc++.h>
+#include<vector>
 #include<algorithm>
 using namespace std;
-void mergesortedarray(int a[],int b[],int n, int m,int k){
-    for(int i=0;i<n;i++){
-         for(int j=0;j<m;j++){
+void mergesortedarray(vector <int> a, vector <int> b){
+  int k=b.size();
+    for(int i=0;i<a.size();i++){
+         for(int j=0;j<b.size();j++){
           if(a[i]>b[j]){
             swap(a[i],b[j]);
           }
          }
-      sort(b,m); 
+         
+      sort(b,k); 
     }
 }
-void print(int a[],int k){
+/*void print(int a[],int k){
     for(int i=0;i<k;i++){
         cout<<a[i]<<" ";
     }
-}
+}*/
 int main(){
-      int n=5,m=4;
-      int a[]={1,2,3,4,5};
-      int b[]={6,7,8,9};
-      int k=n+m;
-      mergesortedarray(a,b,n,m,k);
+      vector <int> a={1,2,3,4,5};
+      vector <int> b={6,7,8,9};
+      mergesortedarray(a,b);
      // print(a,k);
       return 0;
 }
