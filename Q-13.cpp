ea089fab8721bb68ec85b2ec585b0b1ac248eb91
Harshita781 <sharmaharshita431@gmail.com>
2022-10-27 22:09:04 +0530

    initial

--- Q-13.cpp
+++ Q-13.cpp
@@ -1,22 +1,35 @@
- #include<bits/stdc++.h>
- using namespace std;
- long long maxSubarraySum(int arr[], int n){
-        
-       int sum=0;
-       int maxi=arr[0];
-       for(int i=0;i<n;i++){
-           sum=sum+arr[i];
-           maxi=std::max(maxi,sum);
-           if(sum<0){
-               sum=0;
-           }
-       }
-        return maxi;
-    }
+/*
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+Example 1:
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+*/
+#include <bits/stdc++.h>
+using namespace std;
+long long maxSubarraySum(int arr[], int n)
+{
 
-    int main(){
-        int n=5;
-        int arr[]={1,2,3,-2,5};
-        cout<<maxSubarraySum(arr,n);
-        return 0;
+    int sum = 0;
+    int maxi = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+        maxi = std::max(maxi, sum);
+        if (sum < 0)
+        {
+            sum = 0;
+        }
     }
+    return maxi;
+}
+
+int main()
+{
+    int n = 5;
+    int arr[] = {1, 2, 3, -2, 5};
+    cout << maxSubarraySum(arr, n);
+    return 0;
+}
