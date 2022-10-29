a32b53e47009a60a9d8997af4e9f34331495d5ed
Harshita781 <sharmaharshita431@gmail.com>
2022-10-29 21:39:17 +0530

    initial

---
+++ Q-14.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> merged;
+        for(auto interval: intervals){
+            if(merged.empty()|| merged.back()[1]<intervals[0]){
+                merged.push_back(intervals);
+                
+            }
+            else{
+                merged.back()[1]=max(merged.back()[1],intervals[1]);
+            }
+        }
+        return merged;
+    }
+
+int main(){
+    vector<vector<int>> intervals {{1,3},{2,6},{8,10},{15,18}};
+    merge(intervals);
+    return 0;
+}
