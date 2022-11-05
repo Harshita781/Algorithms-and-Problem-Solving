eac1b9ed33a877c0e0bda2eda3d81f1209463352
Harshita781 <sharmaharshita431@gmail.com>
2022-11-05 21:58:09 +0530

    initial

--- Q-14.cpp
+++ Q-14.cpp
@@ -1,7 +1,7 @@
 /*
 Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
- 
+
 
 Example 1:
 
@@ -9,27 +9,31 @@ Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 */
 //#include <bits/stdc++.h>
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>> merged;
-        for(auto interval: intervals){
-            if(merged.empty()|| merged.back()[1] < intervals[0]){
-                merged.push_back(intervals);
-                
-            }
-            else{
-                merged.back()[1]=max(merged.back()[1],intervals[1]);
-            }
+vector<vector<int>> merge(vector<vector<int>> &intervals)
+{
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> merged;
+    for (auto interval : intervals)
+    {
+        if (merged.empty() || merged.back()[1] < intervals[0])
+        {
+            merged.push_back(intervals);
         }
-        return merged;
+        else
+        {
+            merged.back()[1] = max(merged.back()[1], intervals[1]);
+        }
     }
+    return merged;
+}
 
-int main(){
-    vector<vector<int>> intervals {{1,3},{2,6},{8,10},{15,18}};
+int main()
+{
+    vector<vector<int>> intervals{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
     merge(intervals);
     return 0;
 }
