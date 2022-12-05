941f05545b057bdff1f32832f8d2e2c9cea16c79
Harshita781 <sharmaharshita431@gmail.com>
2022-12-05 20:07:24 +0530

    initial

---
+++ Q-20.cpp
@@ -0,0 +1,11 @@
+/*
+Given an array arr of n positive integers and a number k. One can apply a swap operation on the array any number of times, i.e choose any two index i and j (i < j) and swap arr[i] , arr[j] . Find the minimum number of swaps required to bring all the numbers less than or equal to k together, i.e. make them a contiguous subarray.
+
+Example 1:
+
+Input : 
+arr[ ] = {2, 1, 5, 6, 3} 
+K = 3
+Output : 
+1
+*/
