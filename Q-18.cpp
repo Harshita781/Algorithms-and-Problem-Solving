afcf4c305097801d765a63cdf2df7a625f107335
pranjalpandey1800@gmail.com <sharmaharshita431@gmail.com>
2022-12-18 23:04:41 +0530

    question is added

--- Q-18.cpp
+++ Q-18.cpp
@@ -1,3 +1,11 @@
+/*
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+Example 1:
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+*/
 #include <iostream>
 #include <iterator>
 #include <unordered_map>
